4c841850b0358a8b822325e6b098784c50e69d0d
Mayya Matueva <mayamatueva@gmail.com>
2024-02-06 15:16:18 +0100

    all done

--- index.js
+++ index.js
@@ -84,3 +84,100 @@ async function showAllDogsToScreen() {
   document.body.append(ol);
 }
 showAllDogsToScreen();
+
+//Exercise 3
+//3.1
+async function logBooks() {
+  const response = await fetch("https://majazocom.github.io/Data/books.json");
+  const books = await response.json();
+  console.log(books);
+}
+
+logBooks();
+//Array(5)
+
+//3.2
+async function showBooks() {
+  const response = await fetch("https://majazocom.github.io/Data/books.json");
+  const books = await response.json();
+  // filter the books with less than 500 pages
+  const shortBooks = books.filter((book) => book.pages < 500);
+
+  const header = document.createElement("h3");
+  header.textContent = "My Book List";
+  document.body.append(header);
+  // render the short books
+
+  shortBooks.forEach(renderBook);
+}
+
+function renderBook(book) {
+  const div = document.createElement("div");
+  div.textContent = book.title + " - " + book.pages + " pages";
+  document.body.appendChild(div);
+}
+
+showBooks();
+
+//Exercise 4
+//3.1
+async function logVisitors() {
+  const response = await fetch(
+    "https://majazocom.github.io/Data/attendees.json"
+  );
+  const visitors = await response.json();
+  console.log(visitors);
+}
+
+logVisitors();
+//Array(7)
+
+//4.2
+async function showVisitors() {
+  const response = await fetch(
+    "https://majazocom.github.io/Data/attendees.json"
+  );
+  const visitors = await response.json();
+  const attendVisitors = visitors.filter(
+    (visitor) => visitor.attending === true
+  );
+
+  const header = document.createElement("h3");
+  header.textContent = "List of attending visitors";
+  document.body.append(header);
+
+  attendVisitors.forEach(renderVisitor);
+}
+
+function renderVisitor(visitor) {
+  const div = document.createElement("div");
+  div.textContent = visitor.name;
+  document.body.appendChild(div);
+}
+
+showVisitors();
+
+//4.3
+async function showVisitorsWithAllergies() {
+  const response = await fetch(
+    "https://majazocom.github.io/Data/attendees.json"
+  );
+  const visitors = await response.json();
+  const attendVisitors = visitors.filter(
+    (visitor) => visitor.attending === true && visitor.allergies.length > 0
+  );
+
+  const header = document.createElement("h3");
+  header.textContent = "List of attending visitors with allergies";
+  document.body.append(header);
+
+  attendVisitors.forEach(renderVisitor);
+}
+
+function renderVisitor(visitor) {
+  const div = document.createElement("div");
+  div.textContent = visitor.name;
+  document.body.appendChild(div);
+}
+
+showVisitorsWithAllergies();
